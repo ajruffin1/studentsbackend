41443ba41cc4effb84bcdb36af4a7681dbf996a5
ajruffin1 <112420495+ajruffin1@users.noreply.github.com>
2022-09-08 17:13:22 -0400

    Update StudentsController.java

--- demo/src/main/java/com/students/demo/controller/StudentsController.java
+++ demo/src/main/java/com/students/demo/controller/StudentsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.students.demo.model.Students;
 import com.students.demo.repository.StudentsRepository;
 import com.students.demo.resource.StudentsRequest;
-@CrossOrigin(value= "https://studentsbackendmain.herokuapp.com/")
+@CrossOrigin(value= "https://studentsbackendmain.herokuapp.com/students")
 @RestController
 public class StudentsController {
     
